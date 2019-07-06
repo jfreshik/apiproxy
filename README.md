9b911c968b7df2452371dbe1d2fc7e4918b2b110
jfreshik <jfreshik@gmail.com>
2019-07-07 00:14:12 +0900

    WAR configuration for api proxy server

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+# mvn clean compile war:exploded war:war
+```
--- src/main/java/com/example/ApiProxyApplication.java
+++ src/main/java/com/example/ApiProxyApplication.java
@@ -2,9 +2,16 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApiProxyApplication {
+public class ApiProxyApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApiProxyApplication.class);
+    }
 
     public static void main(String[] args){
         SpringApplication.run(ApiProxyApplication.class, args);
