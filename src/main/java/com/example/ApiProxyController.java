cacb57a359ae9a79ceb184e055ffeec4f3a9a5a3
Kim Jongsik <kim.jongsik@hyundai-autoever.com>
2019-07-09 08:43:47 +0900

    dev proxy

--- src/main/java/com/example/ApiProxyController.java
+++ src/main/java/com/example/ApiProxyController.java
@@ -1,22 +1,23 @@
 package com.example;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.HtmlUtils;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.constraints.NotEmpty;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 
 @RestController
 @Validated
@@ -24,35 +25,53 @@ public class ApiProxyController {
 
     @Autowired
     private RestTemplate restTemplate;
-
-    @RequestMapping("/proxy")
-    public ResponseEntity proxy(@RequestBody(required = false) String body,
+    
+    private final String DEV_WAS_IP = "10.7.138.26";
+    private final int DEV_WAS_PORT = 8088;
+    
+    @RequestMapping("/**")
+    public ResponseEntity proxyDev(@RequestBody(required = false) String body,
                                    @RequestHeader HttpHeaders headers,
-                                   @RequestParam @NotEmpty String target,
                                    HttpMethod method,
-                                   HttpServletRequest request)
-            throws URISyntaxException, MalformedURLException {
+                                   HttpServletRequest request) throws URISyntaxException {
+    	
+    	HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
+    	
+    	String queryString = HtmlUtils.htmlUnescape(request.getQueryString());
+    	URI uri = new URI("http", null, DEV_WAS_IP, DEV_WAS_PORT, 
+    			request.getRequestURI(), queryString, null);
 
-        URI uri = buildProxyUri(request, target);
-        HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
-        return proxyRequest(method, uri, httpEntity);
+    	return proxyRequest(method, uri, httpEntity);
     }
 
-    private URI buildProxyUri(HttpServletRequest request, String target)
-            throws MalformedURLException, URISyntaxException {
-        String targetParamUrl = extractTargetUrlFromQueryString(request.getQueryString(), target);
-        String unescapedUrl = HtmlUtils.htmlUnescape(targetParamUrl);
-        return new URL(unescapedUrl).toURI();
-    }
-
-    /**
-     * target 의 query param 이 2개 이상일 때 '&' 가 escape 없이 들어오면 뒤에 param 이 누락됨.
-     * request 의 query string 에서 target 으로 시작하는 위치 부터 문자열 끝까지 target url 로 추출
-     */
-    private String extractTargetUrlFromQueryString(String queryString, String target) {
-        int urlStartIndex = queryString.indexOf(target);
-        return queryString.substring(urlStartIndex);
-    }
+//    @RequestMapping("/proxy")
+//    public ResponseEntity proxy(@RequestBody(required = false) String body,
+//                                   @RequestHeader HttpHeaders headers,
+//                                   @RequestParam @NotEmpty String target,
+//                                   HttpMethod method,
+//                                   HttpServletRequest request)
+//            throws URISyntaxException, MalformedURLException {
+//
+//        URI uri = buildProxyUri(request, target);
+//        HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
+//        return proxyRequest(method, uri, httpEntity);
+//    }
+//
+//    private URI buildProxyUri(HttpServletRequest request, String target)
+//            throws MalformedURLException, URISyntaxException {
+//        String targetParamUrl = extractTargetUrlFromQueryString(request.getQueryString(), target);
+//        String unescapedUrl = HtmlUtils.htmlUnescape(targetParamUrl);
+//        return new URL(unescapedUrl).toURI();
+//    }
+//
+//    /**
+//     * target 의 query param 이 2개 이상일 때 '&' 가 escape 없이 들어오면 뒤에 param 이 누락됨.
+//     * request 의 query string 에서 target 으로 시작하는 위치 부터 문자열 끝까지 target url 로 추출
+//     */
+//    private String extractTargetUrlFromQueryString(String queryString, String target) {
+//        int urlStartIndex = queryString.indexOf(target);
+//        return queryString.substring(urlStartIndex);
+//    }
 
     private ResponseEntity proxyRequest(HttpMethod method, URI uri, HttpEntity<String> httpEntity) {
         try{
