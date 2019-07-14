70905decfa571317ab29f10c69ae81311303148f
Kim Jongsik <kim.jongsik@hyundai-autoever.com>
2019-07-14 11:15:27 +0900

    remove response filter

--- src/main/java/com/example/ApiProxyController.java
+++ src/main/java/com/example/ApiProxyController.java
@@ -1,7 +1,9 @@
 package com.example;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLDecoder;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,6 +14,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -37,18 +40,26 @@ public class ApiProxyController {
     public ResponseEntity proxyDev(@RequestBody(required = false) String body,
                                    @RequestHeader HttpHeaders headers,
                                    HttpMethod method,
-                                   HttpServletRequest request) throws URISyntaxException {
+                                   HttpServletRequest request) throws URISyntaxException, UnsupportedEncodingException {
     	
-    	final String qs = request.getQueryString();
-    	logger.info("queryString: " + qs);
     	HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
     	
-    	String unescapedQs = HtmlUtils.htmlUnescape(qs);
+    	final String qs = refineQueryString(request.getQueryString());
     	URI uri = new URI("http", null, DEV_WAS_IP, DEV_WAS_PORT, 
-    			request.getRequestURI(), unescapedQs, null);
+    			request.getRequestURI(), qs, null);
 
     	logger.info("proxy uri: " + uri.toString());
-    	return proxyRequest(method, uri, httpEntity);
+    	ResponseEntity<String> response = proxyRequest(method, uri, httpEntity);
+    	logger.info("response: " + response.toString());
+    	return response;
+    }
+    
+    private String refineQueryString(final String queryString) throws UnsupportedEncodingException {
+    	logger.info("queryString: " + queryString);
+    	if(StringUtils.isEmpty(queryString)) return null;
+    	final String decodedQs = URLDecoder.decode(queryString, "UTF-8");
+    	String unescapedQs = HtmlUtils.htmlUnescape(decodedQs);
+    	return unescapedQs;
     }
 
 //    @RequestMapping("/proxy")
@@ -80,7 +91,7 @@ public class ApiProxyController {
 //        return queryString.substring(urlStartIndex);
 //    }
 
-    private ResponseEntity proxyRequest(HttpMethod method, URI uri, HttpEntity<String> httpEntity) {
+    private ResponseEntity<String> proxyRequest(HttpMethod method, URI uri, HttpEntity<String> httpEntity) {
         try{
             return restTemplate.exchange(uri, method, httpEntity, String.class);
         } catch(HttpStatusCodeException e) {
